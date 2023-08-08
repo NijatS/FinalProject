1b5ff6259e37efd0dcd55b4265c810d3d761fe79
NijatS <nijatjs@code.edu.az>
2023-08-08 14:12:27 +0400

    fix:responsiveDesign

--- FrontEnd/JavaScript/home.js
+++ FrontEnd/JavaScript/home.js
@@ -2,6 +2,7 @@ const imgs = document.querySelectorAll(".slider img");
 const dots = document.querySelectorAll(".dot");
 const coruselElements = document.querySelectorAll(".carusel-item");
 const carusel = document.querySelector(".carusel");
+const clientColumn = document.querySelector(".client-column");
 let currentImg = 0;
 let i = 0;
 let j = 0;
@@ -29,8 +30,30 @@ function changeSlide(n) {
   }
 }
 function changeCarusel(count) {
+  if (window.screen.width >= 900 && window.screen.width <= 1140) {
+    clientColumn.style.width = "900px";
+    coruselElements.forEach((element) => {
+      element.style.width = "180px";
+      count = coruselElements.length - 5;
+    });
+  } else if (window.screen.width >= 500 && window.screen.width < 900) {
+    clientColumn.style.width = "500px";
+    coruselElements.forEach((element) => {
+      element.style.width = "240px";
+      count = coruselElements.length - 2;
+    });
+  } else {
+    clientColumn.style.width = "1140px";
+    coruselElements.forEach((element) => {
+      element.style.width = "190px";
+      count = coruselElements.length - 6;
+    });
+  }
+  style = getComputedStyle(coruselElements[0]);
+  size = style.width;
+  size = parseInt(size, 10);
   if (i < count) {
-    x = x - 190;
+    x = x - size;
     i++;
   } else if (i == count) {
     x = 0;
