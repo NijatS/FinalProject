cef4bca2d71533e77e4175702b0cd558defb4822
NijatS <nijatjs@code.edu.az>
2023-08-14 17:56:07 +0400

    add:MVCProject

---
+++ BackEnd/Miles/Miles.Data/Context/MilesAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miles.Data.Context
+{
+    public class MilesAppDbContext : DbContext
+    {
+        public MilesAppDbContext(DbContextOptions<MilesAppDbContext> options) : base(options)
+        {
+        }
+    }
+}
