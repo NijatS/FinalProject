56767fd898a0e8efb8c32f4717d0c65c76e34070
NijatS <nijatjs@code.edu.az>
2023-08-14 12:32:58 +0400

    fix:auctionSection

--- FrontEnd/JavaScript/shop-detail.js
+++ FrontEnd/JavaScript/shop-detail.js
@@ -6,10 +6,19 @@ const decreaseBtn = document.querySelector(".nav-prev");
 const increaseBtn = document.querySelector(".nav-next");
 const sections = document.querySelectorAll(".tabs>ul li");
 const infos = document.querySelectorAll(".info");
+const time = document.querySelector(
+  ".car-detail .col-md-4 .info  li:first-child"
+);
+const bidButton = document.querySelector("#bidButton");
+let i = 0;
+setInterval(timeCalculate, 1000);
+const decreaseBit = document.querySelector(".buttons button:first-child");
+const increaseBit = document.querySelector(".buttons button:last-child");
 sImages[0].style.opacity = "1";
 infos[0].style.display = "block";
 sections[0].style.color = "inherit";
 sections[0].style.borderBottom = "2px solid #f4c23d";
+
 for (let i = 0; i < sImages.length; i++) {
   sImages[i].addEventListener("click", () => {
     style = getComputedStyle(slideElements[0]);
@@ -87,4 +96,173 @@ sections.forEach((section) => {
       }
     });
   });
+});
+decreaseBit.addEventListener("click", () => {
+  let number =
+    parseInt(
+      document.querySelector(".bid input").value.split("$")[1].replace(",", "")
+    ) - 500;
+  if (
+    number - 500 <
+    document
+      .querySelector("#base-timer-bid")
+      .textContent.split("$")[1]
+      .replace(",", "")
+  ) {
+    return;
+  }
+  const format = number.toLocaleString("en-US");
+  let text = "$" + format;
+  document.querySelector(".bid input").value = text;
+});
+increaseBit.addEventListener("click", () => {
+  let number =
+    parseInt(
+      document.querySelector(".bid input").value.split("$")[1].replace(",", "")
+    ) + 500;
+
+  const format = number.toLocaleString("en-US");
+  let text = "$" + format;
+  document.querySelector(".bid input").value = text;
+});
+function timeCalculate() {
+  if (time.textContent.split(":")[1] == "0D 0H 0min") {
+    i++;
+    enquiry.children[0].querySelector("h4").textContent = "Live Auction";
+    enquiry.style.margin = "0";
+    enquiry.children[1].style.display = "none";
+    enquiry.children[enquiry.children.length - 1].style.display = "none";
+    enquiry.parentElement.style.flexDirection = "column-reverse";
+    enquiry.parentElement.style.alignSelf = "flex-start";
+    enquiry.parentElement.style.gap = "30px";
+    if (i == 1) {
+      startTimer();
+      document.querySelector(".bid input").value =
+        "$" +
+        (
+          parseInt(
+            document
+              .querySelector("#base-timer-bid")
+              .textContent.split("$")[1]
+              .replace(",", "")
+          ) + 500
+        ).toLocaleString("en-US");
+    }
+  }
+}
+const enquiry = document.querySelector(".enquiry");
+const circle = document.querySelector(".circle");
+const TIME_LIMIT = 20;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+const COLOR_CODES = {
+  info: {
+    color: "green",
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD,
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD,
+  },
+};
+let remainingPathColor = COLOR_CODES.info.color;
+
+circle.innerHTML = `<div class="base-timer">
+<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <g class="base-timer__circle">
+    <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+    <path
+      id="base-timer-path-remaining"
+      stroke-dasharray="283"
+      class="base-timer__path-remaining ${remainingPathColor}"
+      d="
+        M 50, 50
+        m -45, 0
+        a 45,45 0 1,0 90,0
+        a 45,45 0 1,0 -90,0
+      "
+    ></path>
+  </g>
+</svg>
+<span id="base-timer-label" class="base-timer__label" style="display:none">${formatTime(
+  timeLeft
+)}</span>
+<div id = "base-timer-div">
+<span id="base-timer-flag"><img src="../Images/azerbaijan.png"></span>
+<span id="base-timer-country">Azerbaijan</span>
+<span id="base-timer-bid">$22,000</span>
+<span id="base-timer-word">Bid</span>
+</div>
+</div>`;
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+function onTimesUp() {
+  clearInterval(timerInterval);
+}
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML =
+      formatTime(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+function playClick() {
+  let myAudio = document.querySelector("#audio");
+  myAudio.play();
+}
+bidButton.addEventListener("click", () => {
+  timePassed = 0;
+  playClick();
 });
