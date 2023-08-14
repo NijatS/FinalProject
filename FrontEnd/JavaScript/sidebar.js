320dcd1e9daeb23af878dc0eb044e225835929fa
NijatS <nijatjs@code.edu.az>
2023-08-14 16:39:41 +0400

    add:CssToSidebar

--- FrontEnd/JavaScript/sidebar.js
+++ FrontEnd/JavaScript/sidebar.js
@@ -8,11 +8,11 @@ const closeBtn = document.querySelector("#search-item .fa-close");
 const example = document.querySelector(".example");
 sideBarButton.addEventListener("click", (e) => {
   sidebarMenu.style.left = "0px";
-  sidebarMenuPush.style.transform = "translate3d(320px, 0, 0)";
+  sidebarMenuPush.style.transform = "translate3d(300px, 0, 0)";
   sidebarMenuOverlay.style.display = "initial";
   e.stopPropagation();
   sidebarContainer.addEventListener("click", () => {
-    sidebarMenu.style.left = "-250px";
+    sidebarMenu.style.left = "-300px";
     sidebarMenuPush.style.transform = "translate3d(0px, 0, 0)";
     sidebarMenuOverlay.style.display = "none";
   });
