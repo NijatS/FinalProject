6aaac97ae238874e350dd837140b27d8b9dcd35f
NijatS <nijatjs@code.edu.az>
2023-09-02 00:50:08 +0400

    add:swipper

--- BackEnd/Miles/Miles.App/wwwroot/JavaScript/swipper.js
+++ BackEnd/Miles/Miles.App/wwwroot/JavaScript/swipper.js
@@ -1,78 +1,14 @@
-﻿const swiper = new Swiper(".book-swiper", {
-    slidesPerView: 2,
-    spaceBetween: 10,
+﻿const swiper = new Swiper('.sample-slider', {
     loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+    speed: 2000,
+    slidesPerView: 3,
     autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
+        delay: 0,
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-const swiperCompany = new Swiper(".brand-swiper", {
-    // slidesPerView: 4,
-    spaceBetween: 10,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-
-        565: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        // when window width is >= 480px
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        // when window width is >= 640px
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-        },
-    },
-});
-
-const feedback = new Swiper(".feedback-swiper", {
-    slidesPerView: 1,
-    loop: true,
-
-    // spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
- var feedback = new Swiper(".feedback-swiper", {});
+})
+swiper.el.addEventListener('mouseover', function () {
+    swiper.autoplay.stop();
+})
+swiper.el.addEventListener('mouseleave', function () {
+    swiper.autoplay.start();
+})
